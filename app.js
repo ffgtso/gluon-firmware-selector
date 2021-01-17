3e91fb9a5c4736eb4e0a8a314fb13c3a0f57b842
Kai Siering <k.siering@team.mail.de>
2021-01-18 00:02:32 +0100

    Sigh

--- app.js
+++ app.js
@@ -971,7 +971,7 @@ var firmwarewizard = function() {
                       ' (' +prettyPrintVersion(rev.version)+')';
 
         if (rev.branch in config.branch_descriptions) {
-          if(typeof verbyrev[rev.branch] !== 'undefined') {
+          if(verbyrev[rev.branch] != rev.branch) {
           var li = document.createElement('li');
           var name = document.createElement('span');
           name.innerText = rev.branch;
